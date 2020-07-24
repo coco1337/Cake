174ff603958b761e41b485834b94946cf2bddce5
coco1337 <scar3517@gmail.com>
2020-07-25 00:38:29 +0900

    edit player move

---
+++ Assets/Scripts/MapRotateController.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public sealed class MapRotateController : MonoBehaviour
+{
+	[SerializeField] private GameObject map;
+	[SerializeField] private GameObject player;
+	// Update is called once per frame
+	private void Update()
+	{
+		if (Input.GetKeyDown(KeyCode.UpArrow))
+		{
+			map.transform.RotateAround(player.transform.position, Vector3.up, 90f);
+		}
+	}
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public sealed class PlayerController : MonoBehaviour
+{
+	[SerializeField] private CharacterController characterController;
+	[SerializeField] private float moveSpeed;
+	
+	// Start is called before the first frame update
+	void Start()
+	{
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+		characterController.SimpleMove(new Vector3(Input.GetAxis("Horizontal") * moveSpeed, 0, 0));
+	}
+}
