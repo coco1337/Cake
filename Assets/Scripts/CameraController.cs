994f4b4f44863a0e825b122fdbf8ae5689d441e1
qwo330 <qwd330@gmail.com>
2020-07-25 03:49:43 +0900

    기본 맵 회전, 플레이어 이동

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    [SerializeField] Transform mPlayerTrans;
+    [SerializeField] Vector3 Offset = new Vector3(0, 0, 0);
+
+    void LateUpdate()
+    {
+        transform.position = mPlayerTrans.position + Offset;
+    }
+}
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -2,19 +2,39 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(CharacterController))]
 public sealed class PlayerController : MonoBehaviour
 {
-	[SerializeField] private CharacterController characterController;
-	[SerializeField] private float moveSpeed;
-	
-	// Start is called before the first frame update
-	void Start()
-	{
-	}
+    [SerializeField] private CharacterController mCharacterController;
+    [SerializeField] private float mMoveSpeed;
+    [SerializeField] private float mJumpPower;
 
-	// Update is called once per frame
-	void Update()
-	{
-		characterController.SimpleMove(new Vector3(Input.GetAxis("Horizontal") * moveSpeed, 0, 0));
-	}
+    const float mGravity = 9.8f;
+    Vector3 mMoveDir = Vector3.zero;
+
+    void Update()
+    {
+        if (mCharacterController.isGrounded)
+        {
+            if (Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.RightArrow))
+            {
+                mMoveDir = new Vector3(Input.GetAxis("Horizontal"), 0, 0);
+                mMoveDir = transform.TransformDirection(mMoveDir);
+                mMoveDir *= mMoveSpeed;
+            }
+            else // input 없음으로 인한 미끄러짐 방지
+            {
+                mMoveDir = Vector3.zero;
+                mMoveDir = transform.TransformDirection(mMoveDir);
+                mMoveDir *= mMoveSpeed;
+            }
+
+            if (Input.GetButton("Jump"))
+                mMoveDir.y = mJumpPower;
+
+        }
+
+        mMoveDir.y -= mGravity * Time.deltaTime;
+        mCharacterController.Move(mMoveDir * Time.deltaTime);
+    }
 }
