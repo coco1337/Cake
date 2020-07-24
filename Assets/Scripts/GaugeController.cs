2fbe5343b2a98d67b678f534d756c0b383340781
coco1337 <scar3517@gmail.com>
2020-07-25 05:31:11 +0900

    edit sugar high mode in ui

--- Assets/Scripts/GaugeController.cs
+++ Assets/Scripts/GaugeController.cs
@@ -22,17 +22,37 @@ public sealed class GaugeController : MonoBehaviour
 		}
 	}
 
+	private IEnumerator SugarHighMode(int i)
+	{
+		for (int j = 0; gaugeCount - j > 0; ++j)
+		{
+			var inner = gauges[gaugeCount - j - 1].GetComponentsInChildren<Image>()[1];
+			while (inner.fillAmount > 0)
+			{
+				inner.fillAmount -= Time.fixedDeltaTime / 2.5f;
+				yield return null;
+			}
+		}
+	}
+
 	public void IncreaseStrawberryCount()
 	{
 		++strawberryCount;
 		if (strawberryCount <= gaugeCount)
 		{
-			gauges[gaugeCount - strawberryCount].color = Color.black;
+			gauges[gaugeCount - strawberryCount].GetComponentsInChildren<Image>()[1].fillAmount = 1;
 		}
 	}
 
 	public void UseSugarHighMode()
 	{
-		
+		for (int i = 0; gaugeCount - i - 1 > 0; ++i)
+		{
+			if (gauges[gaugeCount - i - 1].GetComponentsInChildren<Image>()[1].fillAmount != 0)
+			{
+				StartCoroutine(SugarHighMode(i));
+				return;
+			}
+		}
 	}
 }
