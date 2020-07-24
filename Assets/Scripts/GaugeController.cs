636d877398cd2bc2fc46761469d19306972d38b5
coco1337 <scar3517@gmail.com>
2020-07-25 01:14:54 +0900

    Add Strawberry prefab, edit gauge UI

---
+++ Assets/Scripts/GaugeController.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public sealed class GaugeController : MonoBehaviour
+{
+	[SerializeField] private int gaugeCount;
+	[SerializeField] private List<Image> gauges;
+	[SerializeField] private Image gaugePrefab;
+	[SerializeField] private GameObject gaugeGroup;
+
+	private int strawberryCount;
+	
+	// Start is called before the first frame update
+	private void Start()
+	{
+		for (int i = 0; i < gaugeCount; ++i)
+		{
+			var gauge = Instantiate(gaugePrefab, gaugeGroup.transform);
+			gauges.Add(gauge);
+		}
+	}
+
+	public void IncreaseStrawberryCount()
+	{
+		++strawberryCount;
+		if (strawberryCount <= gaugeCount)
+		{
+			gauges[gaugeCount - strawberryCount].color = Color.black;
+		}
+	}
+
+	public void UseSugarHighMode()
+	{
+		
+	}
+}
