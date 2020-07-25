68d01c53af52c87138e16ae64cb0950b2fd3dcf0
coco1337 <scar3517@gmail.com>
2020-07-25 22:27:03 +0900

    Revert "Merge branch 'master' into coco"
    This reverts commit 45e6f373a1051f82777356d84789bbb9751a4fc7, reversing
    changes made to 8a7d2bda652a585b625b346051bb784e81778b17.

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -8,19 +8,14 @@ public class GameManager : MonoBehaviour
 	public static GameManager instance;
 	
 	[SerializeField] private Player player;
+	[SerializeField] private GaugeController gaugeController;
 
 	public Player GetPlayer => player;
+	public GaugeController GetGaugeController => gaugeController;
 
 	private void Start()
 	{
 		instance = this;
 		player = GameObject.FindGameObjectWithTag("Player").GetComponent<Player>();
 	}
-
-    public void LoadMap(int level)
-    {
-        Map map = null; // 어떻게 맵 관리할지 미정
-        map.Init();
-
-    }
 }
