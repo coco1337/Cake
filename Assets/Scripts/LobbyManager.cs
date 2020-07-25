4f8643df6caa9bd2669f3923029b3cf5f54e9324
coco1337 <scar3517@gmail.com>
2020-07-26 01:17:08 +0900

    edit Lobby scene

---
+++ Assets/Scripts/LobbyManager.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public sealed class LobbyManager : MonoBehaviour
+{
+	private bool pressed;
+	[SerializeField] private float fadeMul;
+	[SerializeField] private CanvasScaler canvasScaler;
+	[SerializeField] private Image fadeAlpha;
+	
+	private void Update()
+	{
+		if (Input.anyKey && !pressed)
+		{
+			pressed = true;
+			GoToFirstStage();
+		}
+	}
+
+	private void GoToFirstStage()
+	{
+		StartCoroutine(ControlCanvasScale());
+		
+	}
+
+	private IEnumerator ControlCanvasScale()
+	{
+		var p = 33f;
+		while (p > 5)
+		{
+			p -= Time.fixedDeltaTime * fadeMul;
+			canvasScaler.scaleFactor = p;
+
+			yield return null;
+		}
+
+		var q = 0f;
+		while (q <= 1)
+		{
+			q += Time.fixedDeltaTime * 2;
+			fadeAlpha.color = new Color(0, 0, 0, q);
+			yield return null;
+		}
+		
+		SceneManager.LoadScene("Stage1");
+	}
+}
